8cf593f36381b02409cb897ab12a1db4d12c2da0
Avi <12901566+gaviral@users.noreply.github.com>
2025-05-19 15:33:18 -0700

    Add lint and build pre-commit hook

---
+++ .husky/_/husky.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+echo "husky - DEPRECATED\n\nPlease remove the following two lines from $0:\n\n#!/usr/bin/env sh\n. \"$(dirname -- \"$0\")/_/husky.sh\"\n\nThey WILL FAIL in v10.0.0\n"
